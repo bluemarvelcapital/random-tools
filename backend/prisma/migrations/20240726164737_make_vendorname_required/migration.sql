4c0ac2510859888f06961da689a8d8ac2e1f77e0
Dami lola <dami.ijiwola@gmail.com>
2024-07-28 23:11:04 +0100

    separate code base

--- backend/prisma/migrations/20240726164737_make_vendorname_required/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `vendorName` on table `Product` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Product" ALTER COLUMN "vendorName" SET NOT NULL;
--- backend/prisma/migrations/20240727111853_remove_user_relationship_from_vendor_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'vendor';
-
--- AlterTable
-ALTER TABLE "Vendor" ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
